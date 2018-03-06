f39661330d14432a01a752022c6a5948fe724711
dylankb <dylan.barnard@gmail.com>
2018-03-06 11:58:07 -0800

    Copy of Kadence minimal.js example
    When you try and run this it will say that the 'kad' module is missing

---
+++ minimal.js
@@ -0,0 +1,40 @@
+/**
+ * @example kad/example/minimal
+ */
+
+'use strict';
+
+// Import dependencies
+const bunyan = require('bunyan');
+const levelup = require('levelup');
+const kad = require('kad');
+
+// Construct a kademlia node interface; the returned `Node` object exposes:
+// - router
+// - rpc
+// - storage
+// - identity
+const node = kad({
+  transport: new kad.HTTPTransport(),
+  storage: levelup('mydb/storage.db'),
+  contact: { hostname: 'localhost', port: 1337 }
+});
+
+// When you are ready, start listening for messages and join the network
+// The Node#listen method takes different arguments based on the transport
+// adapter being used
+node.listen(1337);
+
+node.join(['ea48d3f07a5241291ed0b4cab6483fa8b8fcc127', {
+  hostname: 'localhost',
+  port: 8080
+}], () => {
+  // Add 'join' callback which indicates peers were discovered and
+  // our node is now connected to the overlay network
+  logger.info(`Connected to ${node.router.length} peers!`)
+
+  // Base protocol exposes:
+  // * node.iterativeFindNode(key, callback)
+  // * node.iterativeFindValue(key, callback)
+  // * node.iterativeStore(key, value, callback)
+});
