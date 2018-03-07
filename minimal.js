d8294ee86e31d4f989e101af13eed1bcabef5ca5
Wilfred Tannr Allard <rockinghamtannr@gmail.com>
2018-03-07 17:55:55 -0500

    Investigating oddities in bucket contacts - getting PING RPC to work

--- minimal.js
+++ minimal.js
@@ -27,9 +27,8 @@ otherNode.listen(1338)
 
 otherNode.join(['ea48d3f07a5241291ed0b4cab6483fa8b8fcc127', {
   hostname: 'localhost',
-  port: 8080
+  port: 8081
 }], () => {
-  otherNode.logger.info(`${otherNode.identity} is my id`)
 })
 
 const node = kad({
@@ -46,10 +45,13 @@ node.listen(1337);
 // otherNode.identity is a Buffer, so you will see it logged as a Buffer below.
 // This is okay, though, because when a string is passed into node.join, it is converted a Buffer
 // if it isn't already a buffer
-node.join([otherNode.identity, {
-  hostname: 'localhost',
-  port: 8080
-}], () => {
+
+node.join([otherNode.identity, { // If we pass otherNode.identity.toString('hex'), it doesn't find
+  hostname: 'localhost',        // otherNode, even though otherNode's string ID version is in fact
+  port: 8082                   // equal to the return value of otherNode.identity.toString('hex')
+}], () => {                     // Buffer.from(otherNode.identity.toString('hex'), 'hex').toString('hex') === otherNode.identity.toString('hex')
+
+
   // Add 'join' callback which indicates peers were discovered and
   // our node is now connected to the overlay network
   
@@ -58,17 +60,41 @@ node.join([otherNode.identity, {
 
   node.router.forEach(bucket => {
     if (bucket.head) {
-      console.log("node knows about: ", bucket) // should show the bucket with otherNode's id in it (will be a Buffer)
+      console.log("node's id is ", node.identity.toString('hex'))
+      console.log("node knows about: ", bucket.keys().next().value.toString('hex')) // should show the bucket with otherNode's id in it (will be a Buffer)
     }
   })
 
+
   otherNode.router.forEach(bucket => {
     if (bucket.head) {
-      console.log("otherNode knows about: ", bucket) // Should show the bucket with node's id in it
+      console.log("OtherNode's ID is ", otherNode.identity.toString('hex'), "----- See, node knows about me")
+      console.log("otherNode knows about: ", bucket.keys().next().value, " ----- but Idk about node") // Should show the bucket with node's id in it
     }
   }) 
+
+  // node knows about otherNode but not vis versa
+  // This is because node uses otherNode's ID to join the network
+  // Which inserts otherNode's ID into the joining node's routing table
+  
+
   // Base protocol exposes:
   // * node.iterativeFindNode(key, callback)
   // * node.iterativeFindValue(key, callback)
   // * node.iterativeStore(key, value, callback)
 });
+
+let contact = null;
+node.router.forEach(bucket => {
+  if (bucket.head) {
+    contact = bucket.head
+  }
+})
+contact[0] = contact[0].toString('hex')
+console.log(contact) // shows otherNode's ID but the hostname and portnumber that node joined on????
+console.log(node.contact) // shows node's contacts correctly
+console.log(otherNode.contact) // shows otherNode's contacts correctly
+node.ping([otherNode.identity.toString('hex'), otherNode.contact], (error, latency) => { 
+  console.log('successful ping?', error, latency)  // expected latency to be a number but it is an empty array...
+
+})
