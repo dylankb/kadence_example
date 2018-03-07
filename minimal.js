66679baeac026104ca77037ff593c0fcb32320a0
Wilfred Tannr Allard <rockinghamtannr@gmail.com>
2018-03-07 13:41:04 -0500

    Adding two nodes - showing their routing tables

--- minimal.js
+++ minimal.js
@@ -16,6 +16,22 @@ const kad = require('@kadenceproject/kadence');
 // - rpc
 // - storage
 // - identity
+
+const otherNode = kad({
+  transport: new kad.HTTPTransport(),
+  storage: levelup(encoding(leveldown('./mydb'))),
+  contact: { hostname: 'localhost', port: 1338}
+})
+
+otherNode.listen(1338)
+
+otherNode.join(['ea48d3f07a5241291ed0b4cab6483fa8b8fcc127', {
+  hostname: 'localhost',
+  port: 8080
+}], () => {
+  otherNode.logger.info(`${otherNode.identity} is my id`)
+})
+
 const node = kad({
   transport: new kad.HTTPTransport(),
   storage: levelup(encoding(leveldown('./mydb'))),
@@ -27,14 +43,30 @@ const node = kad({
 // adapter being used
 node.listen(1337);
 
-node.join(['ea48d3f07a5241291ed0b4cab6483fa8b8fcc127', {
+// otherNode.identity is a Buffer, so you will see it logged as a Buffer below.
+// This is okay, though, because when a string is passed into node.join, it is converted a Buffer
+// if it isn't already a buffer
+node.join([otherNode.identity, {
   hostname: 'localhost',
   port: 8080
 }], () => {
   // Add 'join' callback which indicates peers were discovered and
   // our node is now connected to the overlay network
-  logger.info(`Connected to ${node.router.length} peers!`)
+  
+  node.logger.info(`The nodes I know about: ${node.router.size}`)
+  otherNode.logger.info(`The nodes I know about as otherNode: ${otherNode.router.size}`)
 
+  node.router.forEach(bucket => {
+    if (bucket.head) {
+      console.log("node knows about: ", bucket) // should show the bucket with otherNode's id in it (will be a Buffer)
+    }
+  })
+
+  otherNode.router.forEach(bucket => {
+    if (bucket.head) {
+      console.log("otherNode knows about: ", bucket) // Should show the bucket with node's id in it
+    }
+  }) 
   // Base protocol exposes:
   // * node.iterativeFindNode(key, callback)
   // * node.iterativeFindValue(key, callback)
