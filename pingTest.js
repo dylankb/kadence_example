8905e5e3deee0cc6f11031052b318a4437360769
dylankb <dylan.barnard@gmail.com>
2018-03-08 13:53:10 -0800

    Use async in example

--- pingTest.js
+++ pingTest.js
@@ -10,6 +10,7 @@ const levelup = require('levelup');
 const leveldown = require('leveldown');
 const encoding = require('encoding-down');
 const kad = require('@kadenceproject/kadence');
+const async = require('async');
 
 // Construct a kademlia node interface; the returned `Node` object exposes:
 // - router
@@ -64,29 +65,42 @@ node.router.forEach(bucket => {
 })
 
 // otherNode received a message from node, therefore their mutual routing tables should have updated:
-
-node.ping([otherNode.identity.toString('hex'), otherNode.contact], (error, latency) => {
-  console.log('ping successful?', error, latency)
-})
-
-// otherNode received a message from node, therefore their mutual routing tables should have updated:
 // https://kadence.github.io/KademliaRules.html
 
-node.router.forEach(bucket => {
-  if (bucket.head) {
-    console.log("\nNode's routing table bucket contact - post ping:")
-    console.log("bootstrap node is still head: ", bucket.head)
-    console.log("tail should now be node: ", bucket.tail)
-  }
-})
+// Example using async.series to ensure this isn't an async problem
+// https://caolan.github.io/async/docs.html#series
+
+async.series([
+  function(callback) {
+    node.ping([otherNode.identity.toString('hex'), otherNode.contact], (error, latency) => {
+      console.log('ping successful?', error, latency)
+    })
+    callback();
+  },
+  function(callback) {
+    node.router.forEach(bucket => {
+      if (bucket.head) {
+        console.log("\nNode's routing table bucket contact - post ping:")
+        console.log("bootstrap node is still head: ", bucket.head)
+        console.log("tail should now be node: ", bucket.tail)
+        callback();
+      }
+    })
+  }]
+)
 
 // Also more explicit psuedo-code notes from the kademlia paper
 
 // if (nodeInBucket) { move sending node to tail of the k-bucket/list }
-// else if (!nodeInBucket && bucketHasSpace) { insert sending node at tail }
+// else if (!nodeInBucket && bucketHasSpace) { insert sending node at tail } - this isn't happening
 // else if (!bucketHasSpace) {
 //   response = PING(bucket.head) // ping least-recently seen node
 //   if (response) {
 //     evict least recently seen node, add sending node to tail
 //   } else {
 //     move least recently seen node to tail, discard sending
+
+// If none of the above made sense, basically if node pings otherNode, node should
+// now be in one of otherNodes’ buckets.
+// If node was added to an otherNode bucket that already has a head, then it should
+// be the bucket’s new tail
