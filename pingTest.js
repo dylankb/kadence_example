09b42d1b361c568babf169cac3d50394ad3f386b
dylankb <dylan.barnard@gmail.com>
2018-03-11 13:52:12 -0700

    More updates on ping test

--- pingTest.js
+++ pingTest.js
@@ -28,31 +28,113 @@ otherNode.listen(1338)
 otherNode.join([node.identity, node.contact], () => {
 });
 
-// node should have no contacts at this point
+// node should have no contacts at this point prior to the ping
 console.log(`node router size - pre-ping: ${node.router.size}`);
 
-// Example using async.series to ensure this isn't an async problem
-// https://caolan.github.io/async/docs.html#series
-async.series([
-  function(callback) {
-    node.ping([otherNode.identity.toString('hex'), otherNode.contact], (error, latency) => {
-      console.log('ping successful?', error, latency)
-    })
-    callback();
-    // otherNode received a message from node, therefore their mutual routing tables should have updated:
-    // https://kadence.github.io/KademliaRules.html
-  },
-  function(callback) {
-    console.log(`node router size - post-ping: ${node.router.size}`);
-    node.router.forEach(bucket => {
-      bucket.forEach(contact => {
-        console.log('Post ping contacts?', contact);
-      })
-    })
-  }]
-)
+// Once the ping happens, otherNode will receive a message from node, and their
+// mutual routing tables should update. https://kadence.github.io/KademliaRules.html
+// A way to test this to see if node now has any contacts in the routing table:
 
-// Also more explicit psuedo-code notes from the kademlia paper
+// Doesn't work. First attempt - test in ping callback.
+
+// node.ping([otherNode.identity.toString('hex'), otherNode.contact], (error, latency) => {
+//   console.log('ping successful?', error, latency)
+//   console.log(`node router size - post-ping: ${node.router.size}`); // 0
+//   node.router.forEach(bucket => {
+//     bucket.forEach(contact => {
+//       console.log('Post ping contact: ', contact);
+//     });
+//   });
+// });
+
+// Does not currently work. async#series.
+// https://caolan.github.io/async/docs.html#series
+
+// async.series([
+//   function(callback) {
+//     node.ping([otherNode.identity.toString('hex'), otherNode.contact], (error, latency) => {
+//       callback();
+//       console.log('ping successful?', error, latency)
+//     })
+//   },
+//   function(callback) {
+//     console.log(`node router size - post-ping: ${node.router.size}`); // 0
+//     node.router.forEach(bucket => {
+//       bucket.forEach(contact => {
+//         console.log('Post ping contacts?', contact);
+//       })
+//     })
+//   }]
+// )
+
+// This works. After setTimeout router has a contact.
+
+// node.ping([otherNode.identity.toString('hex'), otherNode.contact], (error, latency) => {
+//   console.log('ping successful?', error, latency)
+//   setTimeout(function() {
+//     console.log(`node router size - post-ping: ${node.router.size}`);
+//     node.router.forEach(bucket => {
+//       bucket.forEach(contact => {
+//         console.log('Post ping contacts?', contact);
+//       })
+//     })
+//   }, 1000)
+// })
+
+// Doesn't work. async / await
+
+// async function testPing(node) {
+//   let promise = new Promise ((resolve, reject) => {
+//     node.ping([otherNode.identity.toString('hex'), otherNode.contact], (error, latency) => {
+//       resolve(node);
+//       console.log('ping successful?', error, latency)
+//     });
+//   });
+//
+//   let updatedNode = await promise;
+//   console.log(`node router size - post-ping: ${node.router.size}`); // 0
+//   updatedNode.router.forEach(bucket => {
+//     bucket.forEach(contact => {
+//       console.log('Post ping contacts?', contact);
+//     })
+//   });
+// }
+//
+// testPing(node);
+
+// Doesn't work. Vanilla JS promise
+
+// var promise = new Promise(function(resolve, reject) {
+//   node.ping([otherNode.identity.toString('hex'), otherNode.contact], (error, latency) => {
+//     console.log('ping successful?', error, latency)
+//   })
+//   resolve(node);
+// });
+// promise.then(function(node) {
+//   console.log('promise param function body');
+  // node.router.forEach(bucket => {
+  //   bucket.forEach(contact => {
+  //     console.log('Post ping contacts?', contact);
+  //   })
+//   })
+
+// Doesn't work. Promise.resolve
+
+// const pingFunc = node.ping([otherNode.identity.toString('hex'), otherNode.contact], (error, latency) => {
+//   console.log('ping successful?', error, latency)
+// })
+// const promise = Promise.resolve(pingFunc)
+//
+// promise.then(function() {
+//   console.log('then param function body');
+  // node.router.forEach(bucket => {
+  //   bucket.forEach(contact => {
+  //     console.log('Post ping contacts?', contact);
+  //   })
+  // })
+// })
+
+// Psuedo-code notes from the kademlia paper
 
 // if (nodeInBucket) { move sending node to tail of the k-bucket/list }
 // else if (!nodeInBucket && bucketHasSpace) { insert sending node at tail } - this isn't happening
@@ -67,3 +149,13 @@ async.series([
 // now be in one of otherNodes’ buckets.
 // If node was added to an otherNode bucket that already has a head, then it should
 // be the bucket’s new tail
+
+// Pseudo-code for what happens when PING RPC is issued
+
+// AbstractNode initialized, add _process as an event handler to on ‘data’ event. https://github.com/kadence/kadence/blob/master/lib/node-abstract.js#L146
+// RPC occurs which fires ‘data’ event (haven't confirmed where)
+// _updateContact called in _proccess https://github.com/kadence/kadence/blob/master/lib/node-kademlia.js#L426
+// _updateContact pushes contact to _updateContactQueue
+// _updateContact queue is queue object created in KademliaNode constructor https://github.com/kadence/kadence/blob/master/lib/node-kademlia.js#L35-L38
+// _updateContactQueue passes contact info to _updateContactWorked
+// _updateContactWorked updates contact in a routing table bucket https://github.com/kadence/kadence/blob/master/lib/node-kademlia.js#L439
